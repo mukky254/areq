1603318f5dbcea70404f55a1677e66382373ab35
mukky254 <muhidinyusuf06@gmail.com>
2025-10-29 15:22:54 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
+  },
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://backita.onrender.com',
   }
 }
 
