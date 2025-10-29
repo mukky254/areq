70c2f485b0f54bc9c522030553ffd78b2a7732ae
mukky254 <muhidinyusuf06@gmail.com>
2025-10-29 09:56:50 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL,
+  }
+}
 
+module.exports = nextConfig
