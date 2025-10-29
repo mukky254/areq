ede14574481102dafd1484edd170980ae4570df6
mukky254 <muhidinyusuf06@gmail.com>
2025-10-29 15:28:48 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -17,11 +17,8 @@ export default function Home() {
   }, [router])
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-green-50">
-      <div className="text-center">
-        <div className="w-16 h-16 border-4 border-blue-600 border-t-transparent rounded-full animate-spin mx-auto mb-4"></div>
-        <p className="text-gray-600">Inapakia...</p>
-      </div>
+    <div className="loading">
+      <div className="spinner"></div>
     </div>
   )
 }
