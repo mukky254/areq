947f299142d5aada9bba0b8d3eb5958b94923c9a
mukky254 <muhidinyusuf06@gmail.com>
2025-10-29 13:23:05 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 }
 
 module.exports = nextConfig
