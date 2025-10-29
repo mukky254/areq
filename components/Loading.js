a8e4f87c2c6e78e20222f3a33a53090fedba0f49
mukky254 <muhidinyusuf06@gmail.com>
2025-10-29 10:32:01 +0300

    Create Loading.js

---
+++ components/Loading.js
@@ -0,0 +1,12 @@
+'use client'
+
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-green-50">
+      <div className="text-center">
+        <div className="w-16 h-16 border-4 border-blue-600 border-t-transparent rounded-full animate-spin mx-auto mb-4"></div>
+        <p className="text-gray-600">Loading...</p>
+      </div>
+    </div>
+  )
+}
